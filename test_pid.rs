78f0420fc1e0fe741e7bc64ef5f0ffb10d525826
Roman Emreis <r.emreis@outlook.com>
2026-03-18 12:01:22 +0400

    chore: remove stray test_pid.rs scratch file

--- test_pid.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    unsafe {
-        let ret = libc::kill(u32::MAX as i32, 0);
-        let errno = *libc::__errno_location();
-        println!("ret={}, errno={}, ESRCH={}, EPERM={}", ret, errno, libc::ESRCH, libc::EPERM);
-    }
-}
