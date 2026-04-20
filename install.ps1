1d3c92d0c90d436e9139bcd4ff4031a86f74ed71
Roman <39233213+RomanEmreis@users.noreply.github.com>
2026-04-20 16:05:23 +0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -12,11 +12,31 @@ $TempDir = ""
 $HasChecksum = $false
 
 function Get-Target {
-    if ([Environment]::Is64BitOperatingSystem) {
-        return "x86_64-pc-windows-msvc"
+    $arch = $env:PROCESSOR_ARCHITECTURE
+    $archWow64 = $env:PROCESSOR_ARCHITEW6432
+
+    if ($archWow64) {
+        $arch = $archWow64
     }
 
-    throw "Only 64-bit Windows is supported"
+    if (-not $arch) {
+        try {
+            $arch = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture.ToString()
+        } catch {
+            $arch = $null
+        }
+    }
+
+    if (-not $arch) {
+        throw "failed to detect Windows architecture"
+    }
+
+    switch ($arch.ToUpperInvariant()) {
+        "AMD64" { return "x86_64-pc-windows-msvc" }
+        "X64"   { return "x86_64-pc-windows-msvc" }
+        "ARM64" { throw "unsupported Windows architecture: ARM64. Supported target: x86_64" }
+        default { throw "unsupported Windows architecture: $arch. Supported target: x86_64" }
+    }
 }
 
 function Resolve-Urls {
