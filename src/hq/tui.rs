e10d1a4be7b7c8e11d8efdf0d71b2a648ce88aa7
Roman <39233213+RomanEmreis@users.noreply.github.com>
2026-04-22 11:26:05 +0400

    Flush Windows console input buffer on TUI resume

--- src/hq/tui.rs
+++ src/hq/tui.rs
@@ -7,13 +7,12 @@ use std::{
 };
 
 use anyhow::Result;
-#[cfg(not(windows))]
-use crossterm::event::{
-    KeyboardEnhancementFlags, PopKeyboardEnhancementFlags, PushKeyboardEnhancementFlags,
-};
 use crossterm::{
     cursor::{MoveDown, MoveToColumn, MoveUp},
-    event::{self, Event, EventStream, KeyCode, KeyEvent, KeyEventKind, KeyModifiers},
+    event::{
+        self, Event, EventStream, KeyCode, KeyEvent, KeyEventKind, KeyModifiers,
+        KeyboardEnhancementFlags, PopKeyboardEnhancementFlags, PushKeyboardEnhancementFlags,
+    },
     queue,
     style::{Attribute, Color, Print, PrintStyledContent, Stylize, style},
     terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode, size},
@@ -969,7 +968,6 @@ fn print_message_and_restore_prompt(
 fn enter_tui() -> Result<()> {
     enable_raw_mode()?;
     let mut stdout = io::stdout();
-    #[cfg(not(windows))]
     let _ = queue!(
         stdout,
         PushKeyboardEnhancementFlags(
@@ -985,7 +983,6 @@ fn enter_tui() -> Result<()> {
 
 fn leave_tui() -> Result<()> {
     let mut stdout = io::stdout();
-    #[cfg(not(windows))]
     let _ = queue!(stdout, PopKeyboardEnhancementFlags);
     let _ = stdout.flush();
     disable_raw_mode()?;
